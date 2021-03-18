7524c918b0d5da6c99a05de8bebcafb918b6018e
Dawas <dawasry.h@hotmail.com>
2021-03-18 16:04:57 +0100

    first commit

---
+++ js/sounds.js
@@ -0,0 +1,47 @@
+var a = document.getElementById("a");
+var b = document.getElementById("b");
+var c = document.getElementById("c"); 
+var d = document.getElementById("d"); 
+var e = document.getElementById("e"); 
+var f = document.getElementById("f"); 
+var g = document.getElementById("g"); 
+var h = document.getElementById("h"); 
+var i = document.getElementById("i"); 
+
+
+
+function sound1() { 
+  a.play(); 
+} 
+
+function sound2() { 
+  b.play(); 
+}
+
+function sound3() { 
+  c.play(); 
+}
+
+function sound4() { 
+  d.play(); 
+}
+
+function sound5() { 
+  e.play(); 
+}
+
+function sound6() { 
+  f.play(); 
+}
+
+function sound7() { 
+  g.play(); 
+}
+
+function sound8() { 
+  h.play(); 
+} 
+
+function sound9() { 
+  i.play(); 
+} 
